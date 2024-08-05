e8e6a28f99267b120fd886663903b3fffe3dfb0b
Muhammad Fahad <muhammadfahadbakkar143@gmail.com>
2024-08-05 15:20:07 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// alert("Testing Alert!")
+
+// var myAlert = "Testing Alert!";
+// console.log(myAlert);
+
+// myAlert = "Alert Change";
+// console.log(myAlert);
+
+// x = 177;
+// y = 189;
+// z = x * y;
+// document.getElementById("test").innerHTML = "The Value of z: " + z;
+
+// a = 10;
+// b = 20;
+// c = b - a;
+// document.getElementById("test").innerHTML = "What is the value of c after the solution?" + c;
+
+// var price01 = 1;
+// let price02 = 2;
+// // const TotalPrice = price01 + price02;
+
+// price01 = 100;
+// price02 = 201;
+// const TotalPrice = price01 + price02
+
+// document.getElementById("test").innerHTML = "The total of price-01 and price-02 is: " + TotalPrice;
+
+var carName = "Range Rover Sports Edition";
+var companyName = "Land Rover";
+var price = "12CR";
+
+document.getElementById("test").innerHTML = "The favorite car is " + carName + ", " + "the company name is " + companyName + " and the price is " + price + "."
+// document.getElementById("test").innerHTML = "The favorite car is " + carName + "," + "<br>" + "the company name is " + companyName + "<br>" + " and the price is " + price + "."
